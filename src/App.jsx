8d9de7ca8c1b8742a0a3d57998a8ac089694dd18
Tony Verbeck <verbeck.tony60@gmail.com>
2024-03-25 14:55:26 +0100

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     const response = await axios.get("http://localhost:3000/");
+
     setData(response.data);
     setIsLoading(false);
   };
@@ -14,6 +16,11 @@ function App() {
   useEffect(() => {
     fetchData();
   }, []);
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <h2>{data.restaurant.name}</h2>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "./assets/logo-teal.svg";
+
+const Header = () => {
+  return (
+    <>
+      <img src="{logo}" alt="" />
+    </>
+  );
+};
+
+export default Header;
